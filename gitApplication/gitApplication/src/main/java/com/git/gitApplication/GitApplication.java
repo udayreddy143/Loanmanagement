89b4dc6adcfe18ae94a8fc315e08418b884125fb
INE12393403 <m.uday@tesco.com>
2025-04-29 21:56:57 +0530

    Ihave added log statement

--- gitApplication/gitApplication/src/main/java/com/git/gitApplication/GitApplication.java
+++ gitApplication/gitApplication/src/main/java/com/git/gitApplication/GitApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitApplication {
 
+
+	System.out.println("hi am uday")
 	public static void main(String[] args) {
 		SpringApplication.run(GitApplication.class, args);
 	}
