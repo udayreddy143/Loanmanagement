0736fba0c74a82919824bd8194eab0b3b6990e3e
INE12393403 <m.uday@tesco.com>
2025-04-29 22:02:50 +0530

    logic has addded

--- gitApplication/gitApplication/src/main/java/com/git/gitApplication/GitApplication.java
+++ gitApplication/gitApplication/src/main/java/com/git/gitApplication/GitApplication.java
@@ -8,6 +8,11 @@ public class GitApplication {
 
 
 	System.out.println("hi am uday")
+
+	if(true) {
+		System.out.println(22);
+		//priyank has to continue this logic
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(GitApplication.class, args);
 	}
